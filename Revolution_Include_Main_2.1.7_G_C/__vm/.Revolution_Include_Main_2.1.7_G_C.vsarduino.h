6138a009d50667218c479f122e60083f1ee5e238
永山 和樹 <kazu@po.harenet.ne.jp>
2017-01-11 00:13:43 +0900

    いろいろな変更

--- Revolution_Include_Main_2.1.7_G_C/__vm/.Revolution_Include_Main_2.1.7_G_C.vsarduino.h
+++ Revolution_Include_Main_2.1.7_G_C/__vm/.Revolution_Include_Main_2.1.7_G_C.vsarduino.h
@@ -65,5 +65,5 @@ typedef void *__builtin_va_list;
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))
-#include "Revolution_include_MoterDriver_1.1.0.ino"
+#include "Revolution_include_Main_2.1.7_G_C.ino"
 #endif
